363da13c34a9e2645f0ff7c09aebcbb5e59d78ea
Vadym Kononenko <vadymk95@gmail.com>
2021-09-20 00:00:07 +0300

    ng directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
 
+export interface Card {
+  title: string;
+  text: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-course';
+  toggle: boolean = true;
+  title: string = 'Angular app';
+
+  cards: Card[] = [
+    {
+      title: 'My favourite title CARD-1!',
+      text: 'some special text for my card',
+    },
+    {
+      title: 'My favourite title CARD-2!',
+      text: 'some special text for my card',
+    },
+    {
+      title: 'My favourite title CARD-3!',
+      text: 'some special text for my card',
+    },
+    {
+      title: 'My favourite title CARD-3!',
+      text: 'some special text for my card',
+    },
+    {
+      title: 'My favourite title CARD-3!',
+      text: 'some special text for my card',
+    },
+  ];
+
+  toggleCards() {
+    this.toggle = !this.toggle;
+  }
 }
